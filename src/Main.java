f85fa588df90045a8959bc2e7756d8a69a8447a9
Epyatakhin <e.pyatakhin@gmail.com>
2023-11-10 01:46:12 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        byte mile = 20;
-        int amount = 15555;
 
-        System.out.println(amount/mile);
+        int bonus = 20;
+        int amount = 15000;
+
+        if (amount >= 20){
+            bonus = amount / bonus;
+        } else {
+            bonus = 0;
+        }
+        int result = amount + bonus;
+        System.out.println("Сумма билета " + amount + " руб.");
+        System.out.println("Количество бонусов " + bonus + " руб.");
+
     }
 }
